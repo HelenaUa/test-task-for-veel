7005b10bb0062384c6c974ae2324431c0a6cec63
HelenaUa <delena.1408@gmail.com>
2025-03-23 20:27:49 +0200

    fixed next.config.mjs for local dev

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  distDir: "out",
+  images: {
+    unoptimized: true, // GitHub Pages не підтримує image optimization
+  },
+  ...(isProd
+    ? {
+        basePath: "/test-task-for-veel",
+        assetPrefix: "/test-task-for-veel",
+      }
+    : {}),
+};
 
 export default nextConfig;
